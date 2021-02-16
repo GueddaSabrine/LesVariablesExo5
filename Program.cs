121c22fa3866e444143bf3771e4e6eaf97c8ac55
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-16 11:12:30 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LesVariablesExo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez votre nom : ");
+            string snom = Console.ReadLine();
+            Console.WriteLine("Entrez votre prénom : ");
+            string sprenom = Console.ReadLine();
+
+            Console.WriteLine($"Bonjour {snom} {sprenom}, nous sommes le jour date mois, comment allez-vous ? ");
+        }
+    }
+}
