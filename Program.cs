5dae111599e5c03ddad3849a824832b2ddb73e00
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-21 12:37:02 +0100

    Vexo5

--- Program.cs
+++ Program.cs
@@ -6,12 +6,18 @@ namespace LesVariablesExo5
     {
         static void Main(string[] args)
         {
+            /*Via la console, demander à l’utilisateur de renseigner son nom et son prénom. 
+            Une fois tous les éléments renseignés, faire en sorte d’afficher dans la console la phrase suivante :
+            « Bonjour Nom Prénom, nous sommes le jour date mois, comment allez - vous ? »*/
+
             Console.WriteLine("Entrez votre nom : ");
             string snom = Console.ReadLine();
             Console.WriteLine("Entrez votre prénom : ");
             string sprenom = Console.ReadLine();
 
-            Console.WriteLine($"Bonjour {snom} {sprenom}, nous sommes le jour date mois, comment allez-vous ? ");
+            DateTime date = DateTime.Today;
+
+            Console.WriteLine($"Bonjour {snom} {sprenom}, nous sommes le {date}, comment allez-vous ? ");
         }
     }
 }
